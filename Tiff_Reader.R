ef7a423be41a747396e8ee81c28397864197d99e
Boyd Ingalls <boyd.ingalls@gmail.com>
2017-04-30 17:44:30 -0500

    updated for raster library

--- Tiff_Reader.R
+++ Tiff_Reader.R
@@ -1,2 +1,5 @@
-Library (tiff)
+library(tiff); library(raster); library(rgdal)
 
+dft <- readTIFF("https://s3.amazonaws.com/share-terravion-com/solanolandtrust/2016-02-25_2310-34_7c98a49f-e6f0-499c-8016-afdac6c58641_Solano-Rush-Ranch-_TIRS_T_4326.tiff", all = TRUE, convert = TRUE)
+
+imgr <- raster("https://s3.amazonaws.com/share-terravion-com/solanolandtrust/2016-02-25_2310-34_7c98a49f-e6f0-499c-8016-afdac6c58641_Solano-Rush-Ranch-_TIRS_T_4326.tiff")
