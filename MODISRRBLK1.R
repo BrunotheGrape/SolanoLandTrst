751cb4afb0fcfab716448172f2a26d7abcebe14d
Boyd Ingalls <boyd.ingalls@gmail.com>
2017-10-29 20:13:47 -0500

    ingest 30 mtr file

--- MODISRRBLK1.R
+++ MODISRRBLK1.R
@@ -12,7 +12,7 @@ ndvi072716 <- raster("https://s3.amazonaws.com/share-terravion-com/solanolandtru
 ndvi081716 <- raster("https://s3.amazonaws.com/share-terravion-com/solanolandtrust/2016-08-17_1759-52_a1e2981e-6840-4c4a-b933-bd262b65b485_Rush-Ranch-Block-1_NDVI_N-R3_4326.tiff")
 ndvi082416 <- raster("https://s3.amazonaws.com/share-terravion-com/solanolandtrust/2016-08-24_2350-38_a1e2981e-6840-4c4a-b933-bd262b65b485_Rush-Ranch-Block-1_NDVI_N-R3_4326.tiff")
 
-MOD2014001 <- raster("https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/1a7bfc10-1cba-41fa-857d-6f4605bb59d7/9799b062-1d94-4997-8908-5a3134032b27/MOD13A1.005__500m_16_days_NDVI_doy2014001_aid0001.tif
+MOD2014001 <- raster("https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/4fad2334-6c11-48b5-a349-836e46b16717/3c51d81b-445e-4ab1-a8f9-79f092f50468/WELDUSWK.001_NDVI_TOA_doy2011232_aid0001.tif
 ")
 
 MOD2014001
@@ -25,3 +25,17 @@ nproj <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "
 MOD2014001trm <- projectRaster(MOD2014001, crs = nproj)
 MOD2014001trm
 image(MOD2014001trm)
+
+
+inCols2 <- ncol(MOD2014001da)
+inRows2 <- nrow(MOD2014001da) 
+image(MOD2014001da)  
+  
+  
+  
+MOD2014001crp <- crop(MOD2014001trm, ndvi050316 )
+image(MOD2014001crp)
+MOD2014001crp
+ndvi050316
+
+
