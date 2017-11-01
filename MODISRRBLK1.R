9a188bf410e9b6e97187dc08b7340c1188830873
Boyd Ingalls <boyd.ingalls@gmail.com>
2017-10-31 21:28:09 -0500

    begin creating stats

--- MODISRRBLK1.R
+++ MODISRRBLK1.R
@@ -4,38 +4,39 @@ library(tiff); library(raster); library(rgdal); library(sp)
 library(ggplot2); library(rasterVis); library(date)
 library(caret); library(dplyr); library(vioplot); library(RColorBrewer)
 
-ndvi050316 <- raster("https://s3.amazonaws.com/share-terravion-com/solanolandtrust/2016-05-03_2123-45_a1e2981e-6840-4c4a-b933-bd262b65b485_Rush-Ranch-Block-1_NDVI_N-R3_4326.tiff")
-ndvi052616 <- raster("https://s3.amazonaws.com/share-terravion-com/solanolandtrust/2016-05-26_2341-38_a1e2981e-6840-4c4a-b933-bd262b65b485_Rush-Ranch-Block-1_NDVI_N-R3_4326.tiff")
-ndvi062316 <- raster("https://s3.amazonaws.com/share-terravion-com/solanolandtrust/2016-06-23_1800-55_a1e2981e-6840-4c4a-b933-bd262b65b485_Rush-Ranch-Block-1_NDVI_N-R3_4326.tiff")
-ndvi062916 <- raster("https://s3.amazonaws.com/share-terravion-com/solanolandtrust/2016-06-29_1659-40_a1e2981e-6840-4c4a-b933-bd262b65b485_Rush-Ranch-Block-1_NDVI_N-R3_4326.tiff")
-ndvi072716 <- raster("https://s3.amazonaws.com/share-terravion-com/solanolandtrust/2016-07-27_1834-13_a1e2981e-6840-4c4a-b933-bd262b65b485_Rush-Ranch-Block-1_NDVI_N-R3_4326.tiff")
-ndvi081716 <- raster("https://s3.amazonaws.com/share-terravion-com/solanolandtrust/2016-08-17_1759-52_a1e2981e-6840-4c4a-b933-bd262b65b485_Rush-Ranch-Block-1_NDVI_N-R3_4326.tiff")
-ndvi082416 <- raster("https://s3.amazonaws.com/share-terravion-com/solanolandtrust/2016-08-24_2350-38_a1e2981e-6840-4c4a-b933-bd262b65b485_Rush-Ranch-Block-1_NDVI_N-R3_4326.tiff")
+RRBLK1SHP <- raster("https://s3.amazonaws.com/share-terravion-com/solanolandtrust/2016-05-03_2123-45_a1e2981e-6840-4c4a-b933-bd262b65b485_Rush-Ranch-Block-1_NDVI_N-R3_4326.tiff")
 
-MOD2014001 <- raster("https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/4fad2334-6c11-48b5-a349-836e46b16717/3c51d81b-445e-4ab1-a8f9-79f092f50468/WELDUSWK.001_NDVI_TOA_doy2011232_aid0001.tif
+MOD2014001 <- raster("https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/252b1f7e-feee-4b70-b34b-1666b57b5fcd/bf6bca53-fbfa-490d-8577-fe23f9e48182/WELDUSWK.001_ACCA_State_doy2003295_aid0001.tif
 ")
 
 MOD2014001
 
-ndvi050316
+#ndvi050316
 image(MOD2014001)
 
+#nproj <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "
+#MOD2014001trm <- projectRaster(MOD2014001, crs = nproj)
+#MOD2014001trm
+#image(MOD2014001trm)
 
-nproj <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 "
-MOD2014001trm <- projectRaster(MOD2014001, crs = nproj)
-MOD2014001trm
-image(MOD2014001trm)
-
-
-inCols2 <- ncol(MOD2014001da)
-inRows2 <- nrow(MOD2014001da) 
-image(MOD2014001da)  
+#inCols2 <- ncol(MOD2014001da)
+#inRows2 <- nrow(MOD2014001da) 
+#image(MOD2014001da)  
   
   
   
-MOD2014001crp <- crop(MOD2014001trm, ndvi050316 )
+MOD2014001crp <- crop(MOD2014001, RRBLK1SHP )
 image(MOD2014001crp)
 MOD2014001crp
-ndvi050316
+MOD2014001crp <- setMinMax(MOD2014001crp)
+MOD2014001crp <- stack(MOD2014001crp)
+
+MOD2014001crp.df <- as.data.frame(MOD2014001crp);colnames(MOD2014001crp.df) <- "NDVI"; MOD2014001crp.df$observation <- 1:nrow(MOD2014001crp.df)
+
+MOD2014001crp.av <- cellStats(MOD2014001crp, mean)
+MOD2014001crp.md <- cellStats(MOD2014001crp, median)
+MOD2014001crp.mx <- cellStats(MOD2014001crp, max)
+MOD2014001crp.mn <- cellStats(MOD2014001crp, min)
+MOD2014001crp.sd <- sd(MOD2014001crp.df$NDVI, na.rm = TRUE)
 
 
