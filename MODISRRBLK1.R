3c7a8a0c128e8662996f24146e8fa46313143a91
Boyd Ingalls <boyd.ingalls@gmail.com>
2017-11-26 17:17:09 -0600

    initial NDVI Stats

--- MODISRRBLK1.R
+++ MODISRRBLK1.R
@@ -4,24 +4,24 @@ library(tiff); library(raster); library(rgdal); library(sp); library(maptools)
 library(ggplot2); library(rasterVis); library(date); library(GISTools)
 library(caret); library(dplyr); library(vioplot); library(RColorBrewer)
 
-#RRBLK1RSTR <- raster("https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/95bf291b-cf11-4b23-9f16-c47d123b0aef/99bc7dfb-9f39-4010-bf4a-f70fcffd6723/WELDUSWK.001_ACCA_State_doy2010260_aid0001.tif")
-#RRBLK1SHP <- rasterToPolygons(RRBLK1RSTR)
+RRBLK1RSTR <- raster("https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/95bf291b-cf11-4b23-9f16-c47d123b0aef/4cd180c9-0468-4cfb-8410-d226b6288a01/WELDUSWK.001_NDVI_TOA_doy2010001_aid0001.tif")
+RRBLK1SHP <- rasterToPolygons(RRBLK1RSTR)
 
-#writeOGR(obj=RRBLK1SHP, dsn="tempdir", layer="torn", driver="ESRI Shapefile") # this is in geographical projection
+writeOGR(obj=RRBLK1SHP, dsn="tempdir", layer="torn", driver="ESRI Shapefile") # this is in geographical projection
 #writeOGR(obj=RRBLK1SHP, dsn="tempdir", layer="torn2", driver="ESRI Shapefile") # this is in equal area projection
 
-rld2003001 <- raster('https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/252b1f7e-feee-4b70-b34b-1666b57b5fcd/5e603ef0-dbf9-4d9b-b995-f83b22cabb24/WELDUSWK.001_ACCA_State_doy2003001_aid0001.tif')
-rld2003001 <- crop(rld2003001, RRBLK1SHP)
-rld2003015 <- raster('https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/252b1f7e-feee-4b70-b34b-1666b57b5fcd/192522bf-6724-439f-adf1-3fb209c3751c/WELDUSWK.001_ACCA_State_doy2003015_aid0001.tif')
-rld2003015 <- crop(rld2003015, RRBLK1SHP)
-rld2003022 <- raster('https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/252b1f7e-feee-4b70-b34b-1666b57b5fcd/083179eb-8fbd-41ce-b1fc-ac125eab364d/WELDUSWK.001_ACCA_State_doy2003022_aid0001.tif')
-rld2003022 <- crop(rld2003022, RRBLK1SHP )
-rld2003036 <- raster('https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/252b1f7e-feee-4b70-b34b-1666b57b5fcd/fbe56db5-d5fa-479a-bcce-598f6fb51ffc/WELDUSWK.001_ACCA_State_doy2003036_aid0001.tif')
-rld2003036 <- crop(rld2003036, RRBLK1SHP)
-rld2003043 <- raster('https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/252b1f7e-feee-4b70-b34b-1666b57b5fcd/9d3c1e91-734b-4d5e-847a-1b4f7e4a82b4/WELDUSWK.001_ACCA_State_doy2003043_aid0001.tif')
-rld2003043 <- crop(rld2003043, RRBLK1SHP)
-rld2003148 <- raster('https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/252b1f7e-feee-4b70-b34b-1666b57b5fcd/212e03c0-cb68-4c48-bb1f-d031161a395b/WELDUSWK.001_ACCA_State_doy2003148_aid0001.tif')
-rld2003148 <- crop(rld2003148, RRBLK1SHP)                     
+#rld2003001 <- raster('https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/252b1f7e-feee-4b70-b34b-1666b57b5fcd/5e603ef0-dbf9-4d9b-b995-f83b22cabb24/WELDUSWK.001_ACCA_State_doy2003001_aid0001.tif')
+#rld2003001 <- crop(rld2003001, RRBLK1SHP)
+                    
+
+modus2010001 <- raster('https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/95bf291b-cf11-4b23-9f16-c47d123b0aef/4cd180c9-0468-4cfb-8410-d226b6288a01/WELDUSWK.001_NDVI_TOA_doy2010001_aid0001.tif')
+modus2010008 <- raster('https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/95bf291b-cf11-4b23-9f16-c47d123b0aef/92cbb7a1-c5c5-4912-8256-dee8a1ec68b8/WELDUSWK.001_NDVI_TOA_doy2010008_aid0001.tif')
+modus2010043 <- raster('https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/95bf291b-cf11-4b23-9f16-c47d123b0aef/0a9f6518-323c-4c92-89ee-d0133deb4518/WELDUSWK.001_NDVI_TOA_doy2010043_aid0001.tif')
+modus2010260 <- raster('https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/95bf291b-cf11-4b23-9f16-c47d123b0aef/cc9bc7af-80de-4537-b0b2-a4ef38fbc1df/WELDUSWK.001_NDVI_TOA_doy2010260_aid0001.tif')
+
+
+cellStats(modus2010260, stat = sd, na.rm = TRUE, asSample = TRUE)
+
 
 rld2010260 <- raster('https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/95bf291b-cf11-4b23-9f16-c47d123b0aef/99bc7dfb-9f39-4010-bf4a-f70fcffd6723/WELDUSWK.001_ACCA_State_doy2010260_aid0001.tif')
 #rld2003148 <- crop(rld2003148, RRBLK1SHP) 
@@ -31,7 +31,7 @@ rld2010260 <- raster('https://lpdaacsvc.cr.usgs.gov/appeears/api/bundle/95bf291b
 r2 <- crop(rld2010260, extent(RRBLK1SHP))
 r3 <- mask(r2, rld2010260)
 
-image(rld2010260)
+image(modus2010260)
 image(r3)
 rld2010260
 r3
